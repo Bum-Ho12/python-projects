fe7522eb504b781babaafa9588e27f522a59e6d9
Bum-Ho12 <bumhonisu@gmail.com>
2023-04-08 17:28:55 +0300

    update with addition to code to open explorer

--- image_text_extractor.py
+++ image_text_extractor.py
@@ -1,13 +1,15 @@
 import pytesseract as T
-import speech_recognition as sr
+# import speech_recognition as sr
 import cv2 as c,os,gtts
 import threading
+import tkinter
+from tkinter import filedialog
 
-def getAudio():
+def getAudio(file_path):
     # initialize tesseract software installed separately from pytesseract
     T.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
     # read image with openCv
-    img = c.imread('test.jpg')
+    img = c.imread(f'{file_path}')
     img = c.resize(img, (720, 480))
     xy = T.image_to_string(img)
     # audio from image text
@@ -15,10 +17,10 @@ def getAudio():
     audio.save("saved_audio.wav")
     os.system("saved_audio.wav")
 
-def showText():
+def showText(file_path):
     # initialize tesseract software installed separately from pytesseract
     T.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-    img = c.imread('test.jpg')
+    img = c.imread(f'{file_path}')
     img = c.resize(img, (720, 480))
     img_height, img_width, _ = img.shape
     # get the boxes
@@ -35,8 +37,10 @@ def showText():
 
 # speech recognition for either text or audio
 def runApp():
-    first_thread = threading.Thread(target=getAudio)
-    second_thread = threading.Thread(target=showText)
+    tkinter.Tk().withdraw() # prevents an empty tkinter window from appearing
+    file_path = filedialog.askopenfile()
+    first_thread = threading.Thread(target=showText(file_path.name))
+    second_thread = threading.Thread(target=getAudio(file_path.name))
     # start th threads
     first_thread.start()
     second_thread.start()
